854799e6eac4d234837fdb612a01cfa658eac917
Michael Boland <michael.boland@mogul.gg>
2020-09-21 19:19:40 -0500

    Change favicon.

--- pages/_document.js
+++ pages/_document.js
@@ -22,39 +22,87 @@ class MyDocument extends Document {
             name="google-site-verification"
           />
           <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-          <link href="/static/favicons/site.webmanifest" rel="manifest" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com/"
             crossOrigin=""
           />
           <link
-            rel="preconnect"
-            href="https://cdn.usefathom.com"
-            crossOrigin=""
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/static/favicons/apple-icon-57x57.png"
           />
           <link
-            href="/static/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/static/favicons/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/static/favicons/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/static/favicons/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/static/favicons/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/static/favicons/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/static/favicons/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/static/favicons/apple-icon-152x152.png"
+          />
+          <link
             rel="apple-touch-icon"
             sizes="180x180"
+            href="/static/favicons/apple-icon-180x180.png"
           />
           <link
-            href="/static/favicons/favicon-32x32.png"
             rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/static/favicons/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
             sizes="32x32"
-            type="image/png"
+            href="/static/favicons/favicon-32x32.png"
           />
           <link
-            href="/static/favicons/favicon-16x16.png"
             rel="icon"
-            sizes="16x16"
             type="image/png"
+            sizes="96x96"
+            href="/static/favicons/favicon-96x96.png"
           />
           <link
-            color="#4a9885"
-            href="/static/favicons/safari-pinned-tab.svg"
-            rel="mask-icon"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
           />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-TileImage"
+            content="/static/favicons/ms-icon-144x144.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
