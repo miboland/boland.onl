b47cd70ebc76acb150f605579c4349cafaa7b845
Michael Boland <michael.boland@mogul.gg>
2020-09-27 19:50:51 -0500

    Fix favicons.

--- pages/_document.js
+++ pages/_document.js
@@ -11,72 +11,10 @@ class MyDocument extends Document {
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           <meta content="#ffffff" name="theme-color" />
-          <meta content="#ffffff" name="msapplication-TileColor" />
-          <meta
-            content="/static/favicons/browserconfig.xml"
-            name="msapplication-config"
-          />
-          <meta content="14d2e73487fa6c71" name="yandex-verification" />
-          <meta
-            content="eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw"
-            name="google-site-verification"
-          />
-          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com/"
-            crossOrigin=""
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="57x57"
-            href="/static/favicons/apple-icon-57x57.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="60x60"
-            href="/static/favicons/apple-icon-60x60.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="72x72"
-            href="/static/favicons/apple-icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="/static/favicons/apple-icon-76x76.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="114x114"
-            href="/static/favicons/apple-icon-114x114.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="120x120"
-            href="/static/favicons/apple-icon-120x120.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="144x144"
-            href="/static/favicons/apple-icon-144x144.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/static/favicons/apple-icon-152x152.png"
-          />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/favicons/apple-icon-180x180.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="192x192"
-            href="/static/favicons/android-icon-192x192.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="icon"
@@ -87,21 +25,12 @@ class MyDocument extends Document {
           <link
             rel="icon"
             type="image/png"
-            sizes="96x96"
-            href="/static/favicons/favicon-96x96.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta
-            name="msapplication-TileImage"
-            content="/static/favicons/ms-icon-144x144.png"
-          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
