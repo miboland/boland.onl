41dda51ee66144fe18ec82467b83903721791a93
Michael Boland <michael.boland@mogul.gg>
2020-09-27 20:05:01 -0500

    Add Safari pinned tab SVG.

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           <meta content="#ffffff" name="theme-color" />
+
+          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="/static/favicons/site.webmanifest" rel="manifest" />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -28,7 +31,11 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            color="#FFFFFF"
+            href="/static/favicons/safari-pinned-tab.svg"
+            rel="mask-icon"
+          />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
           <meta name="theme-color" content="#ffffff" />
