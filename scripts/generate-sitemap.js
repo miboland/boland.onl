7eccf6b49efad73013bec1049374484d99c514fd
Michael Boland <michael.boland@mogul.gg>
2021-02-25 15:55:11 -0600

    Fix sitemap logic

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 
 const globby = require('globby')
-const prettier = require('prettier')(async () => {
+const prettier = require('prettier')
+
+(async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
   const pages = await globby(['pages/**/*{.js,.mdx}', '!pages/_*.js', '!pages/api'])
   const sitemap = `
